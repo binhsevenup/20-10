f92fa6da930f030e5ea533526f1b731f32f29ecd
binhsevenup <43373110+binhsevenup@users.noreply.github.com>
2018-10-30 16:01:41 +0700

    Mp3

---
+++ mp3/content.js
@@ -0,0 +1,96 @@
+var audio1 = {
+id:"1540304588828",
+name:"TÌnh Ca",
+singer:"Trọng Hiếu",
+};
+
+var audio2 = {	
+id:"1540304628575",
+name:"Nếu ",
+singer:"Marzuz",
+};
+
+var audio3 = {
+id:"1540304709939",
+name:"Sài Gòn Tôi Mưa ",
+singer:" Nguyễn Kim Tuyên",
+};
+
+var audio4 = {
+id:"1540307622995",
+name:"Kiss and Make up",
+singer:"Dua Lipa & BLACKPINK",
+};
+
+var audio5 = {
+id:"1540310541059",
+name:"All We Know",
+singer:"The Chainsmokers",
+};
+
+var audio6 = {
+id:"1540310846375",
+name:"Beautifull Now",
+singer:"Zedd",
+};
+
+var audio7 = {
+id:"1540363601379",
+name:"The Monster",
+singer:"Eminem, Rihanna",
+};
+
+var audio8 = {
+id'1540386920750',
+name:"My Love",
+singer:"Westlife",
+};
+
+var audio9 = {
+id:'1540387899963',
+name:"More Than Words",
+singer:"Westlife",
+};
+
+var audio10 = {
+id:'1540388767094',
+name:"Im Your",
+singer:"Jason Mraz",
+};
+
+var listAudio = new Array();
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+listAudio.push(audio10);
+
+classMainContent = document.getElementsByClassName('body');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
